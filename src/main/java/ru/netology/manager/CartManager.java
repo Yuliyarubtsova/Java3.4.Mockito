c5e4cde301f6b5dea8dc728719bea51341822f95
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-02-26 15:59:23 +0800

    Changed project and tests (ten movies)

--- src/main/java/ru/netology/manager/CartManager.java
+++ src/main/java/ru/netology/manager/CartManager.java
@@ -4,6 +4,14 @@ import ru.netology.domain.PurchaseItem;
 
 public class CartManager {
     private PurchaseItem[] items = new PurchaseItem[0];
+    private int moviesLength = 10;
+
+    public CartManager(int moviesLength) {
+        this.moviesLength = moviesLength;
+    }
+
+    public CartManager() {
+    }
 
     public void add(PurchaseItem item) {
         // создаём новый массив размером на единицу больше
@@ -22,28 +30,21 @@ public class CartManager {
     }
 
     public PurchaseItem[] getAll() {
+        if (items.length <= moviesLength) {
         PurchaseItem[] result = new PurchaseItem[items.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
-        for (int i = 0; i < result.length; i++) {
+
+        for (int i = 0; i < items.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
-    }
-
-    // наивная реализация
-    public void removeById(int id) {
-        int length = items.length - 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
-        int index = 0;
-        for (PurchaseItem item : items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
+    } else {
+            PurchaseItem[] result = new PurchaseItem[moviesLength];
+            for (int i = 0; i < moviesLength; i++) {
+                int index = items.length - i - 1;
+                result[i] = items[index];
             }
+            return result;
         }
-        // меняем наши элементы
-        items = tmp;
     }
 }
---
+++ src/test/java/ru/netology/manager/CartManagerTestMoviesLessThanTen.java
@@ -0,0 +1,42 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.PurchaseItem;
+import ru.netology.manager.CartManager;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CartManagerTestMoviesLessThanTen {
+    private CartManager manager = new CartManager(10);
+    private PurchaseItem first = new PurchaseItem(1, 1, "Номер один","Комедия");
+    private PurchaseItem second = new PurchaseItem(2, 2, "Тролли. Мировой тур", "Мультфильм");
+    private PurchaseItem third = new PurchaseItem(3, 3, "Человек-невидимка", "Ужасы");
+    private PurchaseItem fourth = new PurchaseItem(4, 4, "Джентльмены", "Боевик");
+    private PurchaseItem fifth = new PurchaseItem(5, 5, "Отель Белград", "Комедия");
+    private PurchaseItem sixth = new PurchaseItem(6, 6, "Вперед", "Мультфильм");
+    private PurchaseItem seventh = new PurchaseItem(7, 7, "Бладшот", "Боевик");
+
+
+
+    @BeforeEach
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+    }
+
+    @Test
+    public void shouldMoviesAll() {
+
+        PurchaseItem[] actual = manager.getAll();
+        PurchaseItem[] expected = new PurchaseItem[]{seventh, sixth, fifth, fourth, third, second, first};
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
