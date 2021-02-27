642fb7c73a6a291586b3255819de2cc9adf3d522
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-02-27 10:09:20 +0800

    Reformat code

--- src/main/java/ru/netology/manager/Afisha.java
+++ src/main/java/ru/netology/manager/Afisha.java
@@ -14,16 +14,9 @@ public class Afisha {
     }
 
     public void add(PurchaseItem item) {
-        // создаём новый массив размером на единицу больше
         int length = items.length + 1;
         PurchaseItem[] tmp = new PurchaseItem[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
         System.arraycopy(items, 0, tmp, 0, items.length);
-        // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
@@ -31,14 +24,14 @@ public class Afisha {
 
     public PurchaseItem[] getAll() {
         if (items.length <= moviesLength) {
-        PurchaseItem[] result = new PurchaseItem[items.length];
+            PurchaseItem[] result = new PurchaseItem[items.length];
 
-        for (int i = 0; i < items.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
-        }
-        return result;
-    } else {
+            for (int i = 0; i < items.length; i++) {
+                int index = items.length - i - 1;
+                result[i] = items[index];
+            }
+            return result;
+        } else {
             PurchaseItem[] result = new PurchaseItem[moviesLength];
             for (int i = 0; i < moviesLength; i++) {
                 int index = items.length - i - 1;
